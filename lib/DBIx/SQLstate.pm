cc92065e73a9222680eb2f4949f186b22a8b2d24
Theo van Hoesel <tvanhoesel@perceptyx.com>
2023-06-29 10:22:55 +0200

    Add `CONST_PREFIX`

--- lib/DBIx/SQLstate.pm
+++ lib/DBIx/SQLstate.pm
@@ -39,6 +39,7 @@ use warnings;
 our $VERSION = 'v0.0.3';
 
 our $DEFAULT_MESSAGE = 'Unkown SQL-state';
+our $CONST_PREFIX    ='SQLSTATE';
 
 use Exporter qw/import/;
 
@@ -179,6 +180,8 @@ sub constantize ($) {
     $text =~ s/^_|_$//ig;
     
     $text = uc($text);
+    $text = join '_', $CONST_PREFIX, $text
+        if defined $CONST_PREFIX;
     
     return $text;
 }
